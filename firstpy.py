caa17d0742c7109308577f6ca13d5a99197d8e66
pornnakanok <pornnakanok.p@kkumail.com>
2023-12-12 11:06:20 +0700

    function again

--- firstpy.py
+++ firstpy.py
@@ -9,6 +9,8 @@ def parse_input():
         required=True,
         help='Input for the multiplyby9 function'
     )
+    args = parser.parse_args()
+    return args
 
 def printHello():
     print('Hello world!')
