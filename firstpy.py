e9260a19e0fc4bbaf5d41b618670c1261b25aef2
pornnakanok <pornnakanok.p@kkumail.com>
2023-12-12 10:35:03 +0700

    hi

--- firstpy.py
+++ firstpy.py
@@ -1,10 +1,10 @@
 def printHello():
-    print('Hello world')
+    print('Hello world!')
 
 def multiplyby9(inputV):
     print(9*inputV)
 
-if __name__=="main":
-    print('You are in main function')
+if __name__ == "__main__":
+    print('You are in the main function')
     multiplyby9(20)
     printHello()
