3c7e46f5df7810a9320c33f71241b856c13e56c1
pornnakanok <pornnakanok.p@kkumail.com>
2023-12-25 15:47:58 +0700

    fix 3 code

--- python_subprocess.py
+++ python_subprocess.py
@@ -2,7 +2,6 @@ import subprocess #สำหรับรัน terminal command
 
 if __name__ == "__main__":
     #basic terminal command
-    subprocess.run(["ls", "-ltr"])
     subprocess.run(["python", "firstpy.py", "--num", "100", "--xx", "90"])
     subprocess.run(["python", "firstpy.py", "--num", "-10", "--xx", "-90"])
     subprocess.run(["python", "firstpy.py", "--num", "0"])
