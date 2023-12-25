a4336655a1584d31aef20d6d07e293f01b27ad82
pornnakanok <pornnakanok.p@kkumail.com>
2023-12-25 15:33:07 +0700

    3 code

--- python_subprocess.py
+++ python_subprocess.py
@@ -4,4 +4,6 @@ if __name__ == "__main__":
     #basic terminal command
     subprocess.run(["ls", "-ltr"])
     subprocess.run(["rm", "-r", "/home/yoke/testfolder1"])
-    
+    subprocess.run(["python", "firstpy.py" "--num", "66", "--xx", "90"])
+    subprocess.run(["python", "firstpy.py" "--num", "10", "--xx", "50"])
+    subprocess.run(["python", "firstpy.py" "--num", "10"])
