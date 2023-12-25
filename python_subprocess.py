293133cfd51f5aa676f28eb4d5cd5317f3cfa31b
pornnakanok <pornnakanok.p@kkumail.com>
2023-12-25 16:14:22 +0700

    fix 3 code

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,3 +1,4 @@
+from re import sub
 import subprocess #สำหรับรัน terminal command
 
 if __name__ == "__main__":
@@ -11,3 +12,11 @@ if __name__ == "__main__":
     print(f'third run num=0')
     subprocess.run(["python", "firstpy.py", "--num", "0"])
     print(f'-------------------------------------------------------------')
+
+    #use output from other program
+    process_output = subprocess.Popen(["python","firstpy.py", "--num", "0"],
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE)
+    out, err = process_output.communicate()
+    print(out.decode('utf-8'))
+    print(len(out.decode('utf-8')))
