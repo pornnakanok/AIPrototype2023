4db994004fdff965e6e1098e314c17892fcfeebc
pornnakanok <pornnakanok.p@kkumail.com>
2024-01-16 09:39:45 +0700

    name

--- firstclass.py
+++ firstclass.py
@@ -12,9 +12,15 @@ def helloworld():
 def helloyoke():
     return "Hello, Yoke!"
 
-@app.route("/home2")
-def home2():
-    return render_template("home.html",name='Yoke')#,name = 'Tohn', fav = name)
+@app.route("/home", methods=['POST'])
+def homefn():
+    print('We are in home')
+
+    namein = request.form.get('fname')
+    lastnamein = request.form.get('lname')
+    print(namein)
+    print(lastnamein)
+    return render_template("home.html",name='nameid')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True,port=5001) #host='0.0.0.0', port=5001
