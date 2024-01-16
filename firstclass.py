cb9dd7517fa59f7be678be2839acca2b927f5531
pornnakanok <pornnakanok.p@kkumail.com>
2024-01-16 10:50:19 +0700

    add post

--- firstclass.py
+++ firstclass.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, request, render_template, make_response 
 import json
 import sys
@@ -15,13 +16,20 @@ def helloyoke():
 
 @app.route("/home", methods=['POST', 'GET'])
 def homefn():
-    print('We are in home', file=sys.stdout)
+    if request.method == "GET":
+     print('We are in home(GET)', file=sys.stdout)
 
-    namein = request.form.get('fname')
-    lastnamein = request.form.get('lname')
-    print(namein, file=sys.stdout)
-    print(lastnamein, file=sys.stdout)
-    return render_template("home.html",name=namein)
+     namein = request.args.get('fname')
+     print(namein, file=sys.stdout)
+     return render_template("home.html",name=namein)
+    
+    elif request.method == "POST":
+       print('We are in home(POST)', file=sys.stdout)
+       namein = request.form.get('fname')
+       lastnamein = request.form.get('lname')
+       print(namein, file=sys.stdout)
+       print(lastnamein, file=sys.stdout)
+       return render_template("home.html",name=namein)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True,port=5001) #host='0.0.0.0', port=5001
